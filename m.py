0177874c3374c84eac09a9ddad9199584298e46a
Dagar99 <srijanupadhyay999@gmail.com>
2018-07-12 01:23:30 +0530

    update

--- m.py
+++ m.py
@@ -1,8 +1,12 @@
 import numpy as np 
 import tensorflow as tf 
-character=tf.Variable(tf.random_uniform([126,128])
+character=tf.Variable(tf.random_uniform([126,128]))
 def word_to_vec(word):
-      conv1 = tf.layers.conv2d(
+      i=0
+      data=[len(word),128]
+      for i in range(len(word)):
+           data[i]=character[ord(word[i])] 
+      conv1 = tf.layers.conv2d(data,
       input_shape=[None,128],
       filters=32,
       kernel_size=[5, 5],
@@ -12,3 +16,4 @@ def word_to_vec(word):
       f=tf.tanh(tf.add(conv1+bias))
       y=tf.layers.max_pooling2d(t,[2,2],1,padding='same')
       return y
+
