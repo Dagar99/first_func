09707b40a110fdef6cd6680790a68127709f04b7
Dagar99 <srijanupadhyay999@gmail.com>
2018-07-14 12:29:49 +0530

    just update

--- m.py
+++ m.py
@@ -1,6 +1,11 @@
 import numpy as np 
 import tensorflow as tf 
 character=tf.Variable(tf.random_uniform([126,128]))
+Wt=tf.Variable(tf.random_normal([128,128]))
+bt=tf.Variable(tf.zeros([128]))
+Wh=tf.Variable(tf.random_normal([128,128]))
+bh=tf.Variable(tf.zeros([128]))
+
 def word_to_vec(word):
       i=0
       data=[len(word),128]
@@ -17,3 +22,17 @@ def word_to_vec(word):
       y=tf.layers.max_pooling2d(t,[2,2],1,padding='same')
       return y
 
+def t_calc(y):
+      a=tf.matmul(Wt,y)
+      b=tf.add(a,bt)
+      t=tf.sigmoid(b)
+      return t
+def lstm(sentence):
+      lis=sentence.split()
+      i=0
+      for i in range(len(lis)):
+            word=word_to_vec(lis[i])
+            t=t_calc(word)
+            g=tf.nn.relu(tf.add(tf.matmul(Wh,y)+bh))
+            z+=tf.add(tf.multiply(t,g)+tf.multiply((1-t),word))
+      return z
